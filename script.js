eea7c53baf9f040d5be620217c77213c1f21a4f8
analogsoul13 <analogsoul13@gmail.com>
2024-08-22 17:06:16 +0530

    added js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-var screen = document.getElementById('screen');
+let result = document.getElementById('screen')
+
+numClicked=(num)=>{
+    result.value += num
+}
+
+opClicked=(operator)=>{
+    result.value += operator
+}
+
+clearResult=()=>{
+    result.value = ""
+}
+
+calculate=()=>{
+    result.value = eval(result.value)
+}
